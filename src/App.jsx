e025cfa8838fb31437724fcaf3315938c8a487a1
EdFRAX2 <107747563+EdFRAX2@users.noreply.github.com>
2023-04-13 14:49:40 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+import NewForm from './page/NewForm';
+
+
+export default function App() {
+  return (
+    <Container>
+      <NewForm/>
+    </Container>
+  )
+}
